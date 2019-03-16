f1767678feb02371f7e4446538806bd9e6057209
liqingsong <898574371@qq.com>
2019-03-17 00:07:34 +0800

    add dynamic classloader

--- code-enhancer/AsmModifyMethod.java
+++ code-enhancer/AsmModifyMethod.java
@@ -2,6 +2,8 @@ import org.objectweb.asm.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -9,7 +11,6 @@ import static org.objectweb.asm.Opcodes.*;
 
 /**
  * @author liqingsong on 2019/3/9
- * @project javalang-common
  */
 public class AsmModifyMethod {
 
@@ -41,7 +42,8 @@ public class AsmModifyMethod {
         public void visitCode() {
             mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out",
                     "Ljava/io/PrintStream;");
-            mv.visitLdcInsn("hello: method start");
+
+            mv.visitLdcInsn("method start");
             mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "" +
                     "(Ljava/lang/String;)V", false);
             super.visitCode();
@@ -52,7 +54,7 @@ public class AsmModifyMethod {
             if (opcode == ARETURN || opcode == RETURN) {
                 mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out",
                         "Ljava/io/PrintStream;");
-                mv.visitLdcInsn("hello: method end");
+                mv.visitLdcInsn("method end");
                 mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "" +
                         "(Ljava/lang/String;)V", false);
             }
@@ -67,15 +69,63 @@ public class AsmModifyMethod {
 
     }
 
+    public static class CustomizeClassLoader extends ClassLoader {
+        public Class<?> defineClass(String name, byte[] b) {
+            return defineClass(name, b, 0, b.length);
+        }
+    }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, IllegalAccessException,
+            InstantiationException, NoSuchMethodException, InvocationTargetException {
 
+        /**
+         * Use asm operate bytecode
+         */
         String clzName = Bean.class.getName().replace(",", "/");
         ClassReader classReader = new ClassReader(clzName);
         ClassWriter classWriter = new ClassWriter(0);
         LogVisitor logVisitor = new LogVisitor(ASM7, classWriter);
         classReader.accept(logVisitor, ClassReader.SKIP_DEBUG);
         byte[] classBytes = classWriter.toByteArray();
+
+        /**
+         * Dynamically load modified class
+         */
+        CustomizeClassLoader customizeClassLoader = new CustomizeClassLoader();
+
+        Class<?> clazz = customizeClassLoader.defineClass("Bean", classBytes);
+        /**
+         * The new class has the same name as the old one.
+         */
+        System.out.println(clazz.getName());
+
+        /**
+         * Invoke static method without instance. Modified method will be called.
+         */
+        Method staticHelloMethod = clazz.getMethod("hello");
+        staticHelloMethod.invoke(null);
+
+        /**
+         * Bean can't cast to Bean for different classloader
+         */
+        System.out.println(Bean.class.getClassLoader());
+        System.out.println(clazz.getClassLoader());
+
+        /**
+         * This Bean is still old one, and hello still is the old method.
+         */
+        Bean.hello();
+
+        /**
+         * Interface helps resolve classloader issue.
+         */
+        BeanService beanService = (BeanService) clazz.newInstance();
+        beanService.service1();
+
+
+        /**
+         * Another way: create new static class file
+         */
 
         File file = new File("./Bean.class");
         if (!file.exists()) {
